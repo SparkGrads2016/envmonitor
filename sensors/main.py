424308dfde72a52b25b5f04ff24e1bc1a2b01652
Richard See <richard.see@spark.co.nz>
2016-04-14 01:40:07 +0000

    Added time and date to the sensor output

--- sensors/main.py
+++ sensors/main.py
@@ -1,17 +1,18 @@
-import lightSensor
-import tempHumSensor
+import lightSensor as light
+import tempHumSensor as tempHum
 import time
+from time import strftime
 
 pinTempHum = 24 # Set the TempHum pin
 # pinPir = 18     # Set the PIR pin
 
-tsl = lightSensor.initLight() 
+tsl = light.initLight() 
 
 while True:
-    lux, full, ir = lightSensor.getLight(tsl)
-    temp, hum = tempHumSensor.getTempHum(pinTempHum)
-
-    print ('Environmental Readings:')
+    lux, full, ir = light.getLight(tsl)
+    temp, hum = tempHum.getTempHum(pinTempHum)
+    
+    print ('Environmental Readings on ' + strftime("%Y-%m-%d") + ' at ' + strftime("%H:%M:%S"))
     print ('Lux = %d \nFull Spectrum = %d \nIR = %d' % (lux, full, ir))
     print ('Temp = {0:0.1f}* \nHumidity = {1:0.1f}%\n'.format(temp, hum))
     
