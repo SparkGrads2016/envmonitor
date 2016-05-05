2a4662b6104cd20974770ae069789f960f41e5b5
Richard <Richard.See@spark.co.nz>
2016-05-05 15:16:12 +1200

    Made a timer to output data more precisely

--- sensors/main.py
+++ sensors/main.py
@@ -20,9 +20,15 @@ metreConst_x = 103.60
 metreConst_y = 104.70
 metreConst_z = 101.85
 
+# Set how often the monitor will output data
+sleepTime = 10.0
+
 while True:
+	# Process start time
+    t0 = time.time()
 	
-    print ('Start of loop Environmental Readings on ' + strftime("%Y-%m-%d") + ' at ' + strftime("%H:%M:%S") + '\n')
+	# For debugging
+    #print ('Start of loop Environmental Readings on ' + strftime("%Y-%m-%d") + ' at ' + strftime("%H:%M:%S") + '\n')
     
     # Light
     lux, full, ir = light.getLight(tsl2591)
@@ -46,7 +52,14 @@ while True:
     mag_x, mag_y, mag_z = mag
     windBits, windVoltage, windSpeedM, windSpeedKm = wind.getWindSpeed(adcWind)
     
+    # Time to sleep
+    t1 = time.time()		# Process end time
+    t2 = t1 - t0			# Calculate process time to get sensor data
+    sleep = sleepTime - t2	# Calculate the difference in specified sleep time and sensor process time
+    time.sleep(sleep)		# Sleep for that difference
+    
     # Print out variables
+    print ('Environmental Readings on ' + strftime("%Y-%m-%d") + ' at ' + strftime("%H:%M:%S") + '\n')
     print ('Lux = %d \nFull Spectrum = %d \nIR = %d' % (lux, full, ir))
     print ('UV Index = ' + str(uvIndex) + '\n')
     print ('Temperature = {0:0.2f}*C'.format(temperature))
@@ -55,10 +68,14 @@ while True:
     print ('Altitude = {0:0.2f}m'.format(altitude))
     print ('Sealevel Pressure = {0:0.2f}hPa\n'.format(sealevelPressure))
     print ('Accel X = {0:0.2f}m/s^2  Accel Y = {1:0.2f}m/s^2  Accel Z = {2:0.2f}m/s^2'.format(accel_x/metreConst_x, accel_y/metreConst_y, accel_z/metreConst_z))
-    print ('Mag   X = {0}        Mag   Y = {1}        Mag   Z = {2}\n'.format(mag_x, mag_y, mag_z))
+    print ('Mag   X = {0}       Mag   Y = {1}        Mag   Z = {2}\n'.format(mag_x, mag_y, mag_z))
     print ('Bits = {0} Voltage = {1:0.2f}V Wind Speed  = {2:0.3f}m/s Wind Speed = {3:0.3f}km/h\n'.format(windBits, windVoltage, windSpeedM, windSpeedKm))
     
-    print ('End of loop    Now sleeping Readings on ' + strftime("%Y-%m-%d") + ' at ' + strftime("%H:%M:%S"))
-    print ('\n-----------------------------------------------------------------------------\n')
+    # For debugging
+    #print ('End of loop    Now sleeping Readings on ' + strftime("%Y-%m-%d") + ' at ' + strftime("%H:%M:%S"))
     
-    time.sleep(1.0) # Change number to alter how often monitor gets information in seconds
+    print ('-----------------------------------------------------------------------------\n')
+    
+    # For debugging
+    #t3 = time.time()
+    #print ('Total time = {0}'.format(t3-t0))
