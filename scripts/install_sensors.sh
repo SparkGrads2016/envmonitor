6773fef26fb8e4928d2e355bb1f0f1a63cdc2997
pesamino <pesamino.falefata@spark.co.nz>
2016-12-12 05:20:40 +0000

    Test script and install script both working - added code to compensate for infinite test output loops also

--- scripts/install_sensors.sh
+++ scripts/install_sensors.sh
@@ -1,48 +1,79 @@
 #!/bin/bash
 # installing all sensor drivers
- 
+
+cd ..
+sudo mkdir install_files
+cd install_files
 
 sudo apt-get update
 sudo apt-get install build-essential python-pip python-dev python-smbus git
 
+
+
 #Adafruit GPIO - covers SHT31-D temp and humidity sensor
-git clone https://github.com/adafruit/Adafruit_Python_GPIO.git
+sudo git clone  https://github.com/ralf1070/Adafruit_Python_SHT31.git
+sudo git clone https://github.com/adafruit/Adafruit_Python_GPIO.git
 cd Adafruit_Python_GPIO
 sudo python setup.py install
-$TempINS = $?
+tempINS=$?
 cd ..
 
 #TSL2561 Lux sensor
-git clone https://github.com/mgaggero/Adafruit_Python_TSL2561.git
+sudo git clone https://github.com/mgaggero/Adafruit_Python_TSL2561.git
 cd Adafruit_Python_TSL2561
 sudo python setup.py install
-$LuxINS = $?
+luxINS=$?
 cd ..
 
 #BMP180 Barometric pressure, altitude and temperature sensor
-git clone https://github.com/adafruit/Adafruit_Python_BMP.git
+sudo git clone https://github.com/adafruit/Adafruit_Python_BMP.git
 cd Adafruit_Python_BMP
 sudo python setup.py install
-$PressureINS = $?
-#cd examples
-#sudo python simpletest.py
-#cd ..
+pressINS=$?
 cd ..
 
 #Sl1145 UV sensor
-git clone https://github.com/THP-JOE/Python_SI1145.git
+sudo git clone https://github.com/THP-JOE/Python_SI1145.git
 cd Python_SI1145
 sudo python setup.py install
-$UvINS = $?
-#cd examples
-#sudo python simpletest.py
+uvINS=$?
 cd ..
-
 
 #LSM303 Compass and Accelerometer
-sudo apt-get update
-git clone https://github.com/adafruit/Adafruit_Python_LSM303.git
+sudo git clone https://github.com/adafruit/Adafruit_Python_LSM303.git
 cd Adafruit_Python_LSM303
 sudo python setup.py install
-$GyroINS = $?
+gyroINS=$?
 cd ..
+
+printf "\n\n"
+if [ $tempINS -eq 0 ]; then
+        echo "SHT31-D Temp sensor install ... OK"
+else
+        echo "SHT31-D Temp sensor install ... FAIL"
+fi
+
+if [ $luxINS -eq 0 ]; then
+        echo "TSL2561 Lux sensor install ... OK"
+else
+        echo "TSL2561 Lux sensor install ... FAIL"
+fi
+
+if [ $pressINS -eq 0 ]; then
+        echo "BMP180 Pressure sensor install ... OK"
+else
+        echo "BMP180 Pressure sensor install ... FAIL"
+fi
+
+if [ $uvINS -eq 0 ]; then
+        echo "SI1145 UV sensor install ... OK"
+else
+        echo "SI1145 UV sensor install ... FAIL"
+fi
+
+if [ $gyroINS -eq 0 ]; then
+        echo "LSM303 Accelerometer install ... OK"
+else
+        echo "LSM303 Accelerometer install ... FAIL"
+fi
+
--- scripts/test_sensors.sh
+++ scripts/test_sensors.sh
@@ -1,43 +1,51 @@
 #!/bin/bash
 # testing all sensor 
 
-cd ../.. #change into sensor modules directory
+cd ../install_files/ #change into sensor modules directory
 
-printf "TESTING SENSORS...\n" 
+printf "TESTING SENSORS...\n\n" 
 #Adafruit GPIO mod - covers the moduel install of SHT31-D temp and humidity sensor
 cd Adafruit_Python_SHT31
-sudo python Adafruit_SHT31_Example.py > /dev/null 2>&1 #> /dev/null 2>&1 hides output
+sudo python Adafruit_SHT31_Example.py # > /dev/null 2>&1 #> /dev/null 2>&1 hides output
 temp=$?
 cd ..
+
+printf "\n"
 
 #TSL2561 Lux sensor
 cd Adafruit_Python_TSL2561
 cd examples
-sudo python simpletest.py > /dev/null 2>&1
+sudo python simpletest.py #> /dev/null 2>&1
 lux=$?
 cd ../..
+
+printf "\n"
+
 
 #BMP180 Barometric pressure, altitude and temperature sensor
 cd Adafruit_Python_BMP
 cd examples
-sudo python simpletest.py > /dev/null 2>&1
-
+sudo python simpletest.py #> /dev/null 2>&1
 press=$?
 cd ../..
+printf "\n"
 
 #Sl1145 UV sensor
 cd Python_SI1145
 cd examples
-sudo python simpletest.py > /dev/null 2>&1
+sudo timeout 2s python simpletest.py #> /dev/null 2>&1
 UV=$?
 cd ../..
+printf "\n"
 
 #LSM303 Compass and Accelerometer
 cd Adafruit_Python_LSM303
 cd examples
-sudo python simpletest.py > /dev/null 2>&1
+sudo timeout 2s python simpletest.py #> /dev/null 2>&1
 gyro=$?
 cd ../..
+
+printf "\n\n"
 
 if [ $temp -eq 0 ]; then
         echo SHT31-D Temp sensor ... OK
@@ -45,32 +53,30 @@ else
         echo SHT31-D Temp sensor ... FAIL
 fi
 
-
 if [ $lux -eq 0 ]; then
         echo TSL2561 Lux sensor ... OK
 else
         echo TSL2561 Lux sensor ... FAIL
 fi
 
-
 if [ $press -eq 0 ]; then
         echo BMP180 Pressure sensor ... OK
 else
         echo BMP180 Pressure sensor ... FAIL
 fi
-
-
-if [ $UV -eq 0 ]; then
+#the below tests run infinte loops therefore a timeout limit of 2s is used.
+#when timeout successfully reaches limit a status code of 124 appears
+#tests will never exit with status code 0 as it never fully completes 
+if [ $UV -eq 124 ]; then
         echo SI1145 UV sensor ... OK
 else
         echo SI1145 UV sensor ... FAIL
 fi
 
-
-if [ $gyro -eq 0 ]; then
-        echo LSM303 Gyro sensor ... OK
+if [ $gyro -eq 124 ]; then
+        echo LSM303 Accelerometer sensor ... OK
 else
-        echo LSM303 Gyro sensor ... FAIL
+        echo LSM303 Accelerometer sensor ... FAIL
 fi 
 
-printf "...TESTING DONE\n"
+printf "\n...TESTING DONE\n"
